7dfa03231a95382b08a0623eb8fc29f79fe63b79
Denis Naughton <mjolinor75@hotmail.com>
2020-08-30 20:28:53 -0400

    first commit

---
+++ Warcardgame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Warcardgame
+//
+//  Created by Admin on 8/30/20.
+//  Copyright © 2020 DASHtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
